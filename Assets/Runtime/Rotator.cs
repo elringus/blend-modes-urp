dbeee189b3b101ec45e4f4bd943dd99c01f95acf
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-06 11:22:56 +0300

    Initial commit

---
+++ Assets/Runtime/Rotator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float speed = 30f;
+    [SerializeField] private Vector3 axis = new Vector3(1, 1, 1);
+
+    private void Update ()
+    {
+        transform.Rotate(axis, speed * Time.deltaTime);
+    }
+}
